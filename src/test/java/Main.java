47ab748d8afed1bf98223c591de816a71924505a
Mishail <Marvin721@yandex.ru>
2026-03-30 11:44:04 +0300

    commit

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
     private static String print = "Hello qa.guru!";
 
     public static void main(String[] args) {
-        System.out.println("Hello QAA");
-        System.out.println("Giv");
+        System.out.println("Hello Tags");
+        System.out.println("GitCommit");
     }
 }

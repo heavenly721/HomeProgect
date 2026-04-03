d2c399c859f80b7c706e87fd813566a52776160b
Mishail <Marvin721@yandex.ru>
2026-04-03 13:46:35 +0300

    commit1

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -7,5 +7,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Tags");
         System.out.println("GitCommit");
+        System.out.println("NewYou");
     }
 }
